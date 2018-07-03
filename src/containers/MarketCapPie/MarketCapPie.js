890010b99d618a4e87eac6154b2890a49b55e50b
Daniel Daukševič <danieldauk@gmail.com>
2018-07-03 18:25:42 +0300

    pie chart 2

--- src/containers/MarketCapPie/MarketCapPie.js
+++ src/containers/MarketCapPie/MarketCapPie.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as d3 from 'd3';
+import d3tip from 'd3-tip';
 import './MarketCapPie.css';
 
 class MarketCapPie extends Component {
   componentDidMount() {
+    const tip = d3tip(d3)
+      .attr('class', 'd3-tip')
+      .html(d => `
+      <span><strong>Market Cap: </strong>${d3.format(',.8r')(d.value)}</span></br>
+      <span><strong>Global Market Cap: </strong>${((d.value * 100) / totalMarketCap).toFixed(2)}%</span>
+      `);
     const { totalMarketCap, currentMarketCap } = this.props;
 
     const data = [
       {
-        value: totalMarketCap,
+        value: totalMarketCap - currentMarketCap,
         color: '#2066A3',
       },
       {
@@ -20,21 +27,21 @@ class MarketCapPie extends Component {
 
     // dimensions
 
-    const width = 200;
-    const height = 200;
+    const width = 160;
+    const height = 160;
     const radius = width / 2;
 
     // arc generator
     const arc = d3.arc()
-      .outerRadius(radius - 20)
-      .innerRadius(radius / 2);
+      .outerRadius(radius - 2)
+      .innerRadius(radius - 22);
 
     const pie = d3.pie()
       .sort(null)
       .value(d => d.value);
 
     // appending svg
-    const svg = d3.select('#pie-chart')
+    const svg = d3.select('.pie-chart')
       .append('svg')
       .attr('width', width)
       .attr('height', height)
@@ -47,14 +54,35 @@ class MarketCapPie extends Component {
       .enter().append('g')
       .attr('class', 'arc');
 
+    // append tooltip
+    svg.select('.arc:last-child').on('mouseover', tip.show)
+      .on('mouseout', tip.hide);
+
     g.append('path')
-      .attr('d', arc)
-      .style('fill', d => d.data.color);
+      .style('fill', d => d.data.color)
+      .transition().delay((d, i) => i * 500)
+      .duration(500)
+      .attrTween('d', (d) => {
+        const i = d3.interpolate(d.startAngle + 0.1, d.endAngle);
+        return function (t) {
+          d.endAngle = i(t);
+          return arc(d);
+        };
+      });
+
+    g.call(tip);
   }
 
+  // TODO: add tooltip
   render() {
     return (
-      <div id="pie-chart" />
+      <div>
+        <div className="pie-chart" />
+        <h3 className="pie-chart__title">
+          Market Cap
+        </h3>
+      </div>
+
     );
   }
 }
